8d5493a35bbcfe7b0b196ab0e2c49f64bb9d045d
Che-Fu Steven Chang <36339155+hkazami@users.noreply.github.com>
2022-03-02 15:34:24 +0800

    Create profiler.py

---
+++ pyhandy/profiler.py
@@ -0,0 +1,26 @@
+import cProfile
+import io
+import pstats
+
+
+def get_profiler():
+    return cProfile.Profile()
+    
+class Profiler(object):
+    def __init__(self, filepath):
+        self.cprofiler = cProfile.Profile()
+        self.filepath = filepath
+    
+    def start(self):
+        self.cprofiler.enable()
+    
+    def stop(self):
+        self.cprofiler.disable()
+        self.cprofiler.dump_stats(self.filepath)
+        
+        s = io.StringIO()
+        stats = pstats.Stats(self.cprofiler, stream=s).sort_stats('cumtime')
+        stats.print_stats()
+        with open(filepath.split('.')[0] + '.prof', 'w+') as f:
+            f.write(s.getvalue())
+
